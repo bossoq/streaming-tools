6c27582a6a054e0abb424588c6d998dd173fc392
Kittipos Wajakajornrit <boss.zz@me.com>
2022-06-26 23:27:49 +0700

    Check market status for coin

--- bot/twitch/commands/coin.ts
+++ bot/twitch/commands/coin.ts
@@ -4,6 +4,13 @@ import type { TwitchCommand } from '../types'
 const coin: TwitchCommand = {
   name: '!coin',
   execute: async (_client, channel, _user, _message, tag, misc) => {
+    const marketOpen =
+      (await misc?.redis?.hGet('twitchBotStat', 'market')) === 'open'
+    const checkRole =
+      tag.userInfo.isBroadcaster ||
+      tag.userInfo.isMod ||
+      tag.userInfo.isSubscriber
+    if (!marketOpen && !checkRole) return
     const user = await prisma.userInfo.findUnique({
       select: { coin: true },
       where: { twitchId: tag.userInfo.userId }
