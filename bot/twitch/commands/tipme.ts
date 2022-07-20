c2b2d4b7e1fede4dc1bb1a7c16b91ef7f5aa47f1
Kittipos Wajakajornrit <boss.zz@me.com>
2022-07-20 21:17:17 +0700

    Update tipme url

--- bot/twitch/commands/tipme.ts
+++ bot/twitch/commands/tipme.ts
@@ -3,8 +3,7 @@ import type { TwitchCommand } from '../types'
 const discord: TwitchCommand = {
   name: ['!tip', '!tipme'],
   execute: async (_client, channel, _user, _message, tag, misc) => {
-    const tipmeUrl =
-      process.env.TIPME_LINK || 'https://tipme.in.th/9c7b073691de260013ea2906'
+    const tipmeUrl = process.env.TIPME_LINK || 'https://tipme.in.th/sniffslive'
     await misc?.sendMessage!(
       channel,
       `${tag.userInfo.displayName} ให้อาหารแมวได้ที่ ${tipmeUrl} sniffsBaby`
